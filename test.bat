2038b650b6c9350e9805f32320f58d1eaa43176b
Rokit00 <simple3367@gmail.com>
2024-05-30 17:49:07 +0900

    new

--- test.bat
+++ test.bat
@@ -3,10 +3,10 @@ set "FOLDER_PATH=C:\Users\biznbank\Downloads\RFC_SOCKET"
 if exist "%FOLDER_PATH%" (
     rmdir /s /q "%FOLDER_PATH%"
     if exist "%FOLDER_PATH%" (
-        echo 폴더 삭제 실패: %FOLDER_PATH%
+        echo  %FOLDER_PATH%
     ) else (
-        echo 폴더가 삭제되었습니다.
+        echo f
     )
 ) else (
-    echo 폴더를 찾을 수 없습니다.
+    echo ff
 )
