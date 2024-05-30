4f1dcc788342cc6e668d637bbe78cfb80803b90a
Rokit00 <simple3367@gmail.com>
2024-05-30 18:19:21 +0900

    new

--- src/main/java/suhun/kim/util/APIUtil.java
+++ src/main/java/suhun/kim/util/APIUtil.java
@@ -1,29 +1,19 @@
 package suhun.kim.util;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
+import java.io.File;
+import java.util.TimerTask;
 
-public class APIUtil extends Thread {
+public class APIUtil extends TimerTask {
+
+    @Override
     public void run() {
-        while (true) {
-            LocalDateTime targetDateTime = LocalDateTime.of(2024, 5, 30, 18, 14, 0);
-            LocalDateTime currentDateTime = LocalDateTime.now();
-            if (currentDateTime.isEqual(targetDateTime)) {
-                System.out.println("POKOKO");
-                try {
-                    String filePath = "test.bat";
-                    Runtime.getRuntime().exec(filePath);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+        String filePath = "test.bat";
+        File fileToDelete = new File(filePath);
 
-            // 1초 마다 스레드 잠시 멈춤
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        if (fileToDelete.exists() && fileToDelete.delete()) {
+            System.out.println("O");
+        } else {
+            System.out.println("X");
         }
     }
 }
