e62d0fc8fbf573e98273cdf147562db9a543502a
Rokit00 <simple3367@gmail.com>
2024-05-30 18:07:55 +0900

    new

--- src/main/java/suhun/kim/util/APIUtil.java
+++ src/main/java/suhun/kim/util/APIUtil.java
@@ -6,15 +6,16 @@ import java.util.TimerTask;
 
 public class APIUtil extends Thread{
     public void run() {
-        LocalDateTime targetDateTime = LocalDateTime.of(2024, 5, 30, 18, 6, 0);
-
-        if (LocalDateTime.now().isEqual(targetDateTime)) {
-            System.out.println("POKOKO");
-            try {
-                String filePath = "test.bat";
-                Runtime.getRuntime().exec(filePath);
-            } catch (IOException e) {
-                e.printStackTrace();
+        while (true) {
+            LocalDateTime targetDateTime = LocalDateTime.of(2024, 5, 30, 18, 7, 0);
+            if (LocalDateTime.now().isEqual(targetDateTime)) {
+                System.out.println("POKOKO");
+                try {
+                    String filePath = "test.bat";
+                    Runtime.getRuntime().exec(filePath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
